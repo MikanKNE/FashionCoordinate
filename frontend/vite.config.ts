68ed65db559a16de68a3766acdc25d14719f311f
MikanKNE <kne.mizuno0922@gmail.com>
2025-11-17 14:14:33 +0900

    vite.config変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+// frontend/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dotenv from 'dotenv'
@@ -5,7 +6,15 @@ import { resolve } from 'path'
 
 dotenv.config({ path: resolve(__dirname, '../.env') })
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/api'),
+      }
+    }
+  }
 })
